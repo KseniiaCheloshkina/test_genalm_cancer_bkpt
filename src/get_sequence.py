55c9bcc8bf7d8c5b49b8f5caa1d2f84a2eea6a3e
Kseniia Cheloshkina <kscheloshkina@gmail.com>
2023-07-15 12:12:48 +0300

    add new files

---
+++ src/get_sequence.py
@@ -0,0 +1,7 @@
+# genome hg38 GenBank	 https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_000001405.26/
+# from pyfaidx import Fasta
+# seq = Fasta("data/ncbi_dataset/ncbi_dataset/data/GCA_000001405.15/GCA_000001405.15_GRCh38_genomic.fna")
+# import pybedtools
+
+# curl http://genome.ucsc.edu/cgi-bin/das/hg38/dna?segment=chr1:4321,4330
+# curl -L 'https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr1;start=4321;end=4330'
