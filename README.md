70984b1743fc43b84cbae0906e6a814d18c4e669
Kseniia Cheloshkina <kscheloshkina@gmail.com>
2023-07-29 14:01:38 +0300

    update scripts to get dna sequence

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 ## Data Sources
 Breakpoints data - https://github.com/KseniiaCheloshkina/cancer_breakpoints_hotspots_prediction/tree/master/data/raw%20breakpoints
-Genome sequence - https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_000001405.26/
+Genome sequence - https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz (Dec 2013)
 Chromosome lengths - https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_000001405.26/
 
-Methods to get DNA sequence by coordinates in genome:
-* pyfaidx:
-```python
-from pyfaidx import Fasta
-seq = Fasta("data/ncbi_dataset/ncbi_dataset/data/GCA_000001405.15/GCA_000001405.15_GRCh38_genomic.fna")
-# awk '{print $1,$2-1, $2}' records.txt | xargs faidx hg19.fa --bed - --transform transposed
+Get DNA sequence by coordinates in genome using bedtools:
+```bash
+apt-get update
+apt-get install bedtools
+wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+gzip -d hg38.fa.gz
+bedtools getfasta -fi hg38.fa -bed positive_all_cancers_4000.bed -tab -fo pos_4000.bed
 ```
-* pybedtools
+BED format: "zero-based, half-open" (coordinate of the first nucleotide of a chromosome is defined as 0, "End" coordinate is not part of the feature)
+
+Other methods (curl):
 * ucsc
 ```bash
 curl http://genome.ucsc.edu/cgi-bin/das/hg38/dna?segment=chr1:4321,4330
--- src/generate_windows.py
+++ src/generate_windows.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 from typing import Tuple
 import requests
 import pandas as pd
@@ -17,44 +18,35 @@ def generate_window(chrom: str, pos: int, win_len: int) -> Tuple[int, int]:
 
 
 def get_sequence(chrom: str, start: str, end: str) -> str:
+    # It takes a lot of time ona big dataset...
     addr = f"https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr{chrom};start={start};end={end}"
     answ = requests.post(addr).json()["dna"]
-    # TODO: check if its needed to set it to lower or upper case
     return answ
 
 
-def get_positive_windows(csv_path: str, win_len: int) -> None:
+def get_positive_windows(csv_path: str, win_len: int) -> pd.DataFrame:
     df = pd.read_csv(csv_path)
     all_seq = []
     for _, bkpt in tqdm.tqdm(df.iterrows()):
         win_start, win_end = generate_window(
             chrom=bkpt["chr"], pos=int(bkpt["start"]), win_len=win_len
         )
-        # TODO: fix
-        # dna_seq = get_sequence(
-        #     chrom=bkpt["chr"], start=str(win_start), end=str(win_end)
-        # )
-        dna_seq = "a"
-        all_seq.append((win_start, win_end, dna_seq))
+        all_seq.append((win_start, win_end))
     df["win_start"] = [el[0] for el in all_seq]
-    df["win_end"] = [el[1] for el in all_seq]
-    df["dna_seq"] = [el[2] for el in all_seq]    
-    df = df[["chr", "start", "win_start", "win_end", "dna_seq", "cancer_type"]]
+    df["win_end"] = [el[1] for el in all_seq]   
+    df = df[["chr", "start", "win_start", "win_end", "cancer_type"]]
     df = df.rename(columns={"start": "position", "chr": "chromosome"})
     df["label"] = 1
-    print("With N: ", df[df["dna_seq"].str.contains("N")].shape[0])    
-    df.to_csv(csv_path.replace(".csv", f"_{win_len}.csv"))
     return df
 
 
 def get_negative_windows(n_points: int, win_len: int) -> pd.DataFrame:
-    # output: chromosome, win_start, win_end, position, dna_seq, label
     with open("data/chr_lengths.json", "r") as f:
         chr_lengths = json.load(f)
     mean_telomeres_len = 10000
     num_points_per_chr = round(n_points / len(chr_lengths))
     all_points = []
-    for chrom, chr_length in chr_lengths.items():
+    for chrom, chr_length in tqdm.tqdm(chr_lengths.items()):
         positions = np.random.uniform(
             low=mean_telomeres_len,
             high=chr_length - mean_telomeres_len,
@@ -65,15 +57,15 @@ def get_negative_windows(n_points: int, win_len: int) -> pd.DataFrame:
         values = []
         for pos in positions:
             win_start, win_end = generate_window(chrom, pos, win_len)
-            dna_seq = get_sequence(chrom=chrom, start=str(win_start), end=str(win_end))
-            values.append((win_start, win_end, dna_seq))
+            values.append((win_start, win_end))
         df["win_start"] = [el[0] for el in values]
         df["win_end"] = [el[1] for el in values]
-        df["dna_seq"] = [el[2] for el in values]
         all_points.append(df)
     df_all = pd.concat(all_points)
+    df_all = df_all.sort_values(['chromosome', 'win_start'])
     df_all["label"] = 0
     return df_all
+
 
 
 if __name__ == "__main__":
@@ -82,7 +74,24 @@ if __name__ == "__main__":
         "--win_len", help="length of window to generate", default=512, type=int
     )
     args = parser.parse_args()
-    df_new = get_positive_windows(csv_path="data/breakpoints_wo_bad_regions.csv", win_len=args.win_len)
-    # df_new = get_negative_windows(n_points=40, win_len=args.win_len)
-    # print(df_new.head())
-    # df_new.to_csv("data/tmp.csv")
+    
+    main_path = "data/dataset/"
+    # save positive
+    df_pos = get_positive_windows(csv_path="data/breakpoints_wo_bad_regions.csv", 
+                                  win_len=args.win_len)
+    df_pos.to_csv(f"{main_path}positive_all_cancers_{args.win_len}.csv")
+    # generate_negative
+    df_neg = get_negative_windows(n_points=1000000, win_len=args.win_len)
+    df_neg.to_csv(f"{main_path}negative_all_cancers_{args.win_len}.csv")
+    
+    # save to ved format to finally get DNA sequence
+    flnms = [fl for fl in os.listdir(main_path) if fl.endswith(".csv")]
+    for fl in flnms:
+        df_windows = pd.read_csv(os.path.join(main_path, fl))
+        df_windows['chromosome'] = df_windows['chromosome'].map(lambda x: 'chr' + str(x))
+        df_windows[['chromosome', 'win_start', 'win_end']].to_csv(
+            os.path.join(main_path, fl.replace(".csv", ".bed")), 
+            sep="\t", 
+            header=False,
+            index=False
+        )
